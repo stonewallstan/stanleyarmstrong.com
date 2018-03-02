e4d5b6b2ec16a7d8e04cf6ebb16a0c36c3815a97
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2018-03-02 12:41:11 -0800

    Setting Up a React Environment

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>stanleyarmstrong.com</div>
+        );
+    }
+}
+
+export default App;
