2367ee21f6935841537dd69a3682b49ba412e855
stonewallstan <stanleyarmstrong31@gmail.com>
2018-07-08 00:32:25 -0700

    Need one more component on the home page

--- react-client/src/Components/App.jsx
+++ react-client/src/Components/App.jsx
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { browserHistory } from 'react-router';
 import HomePage from './HomePage';
-import NavBar from './HeaderComponent/NavBar';
+import NavBar from './NavComponents/NavBar';
 import Footer from './footerComponent/Footer';
 import MediaPage from './MediaPage';
 import TechPage from './TechPage';
+import MoreNavigation from './NavComponents/MoreNavigation'
 
 
 class App extends Component {
@@ -17,6 +18,7 @@ class App extends Component {
           <Route name = "home" exact path="/" component={HomePage} />
           <Route name = "media" exact path="/media" component={MediaPage} />
           <Route name = "tech" exact path="/tech" component={TechPage} />
+          <Route name = "buttons" exact path="/" component={MoreNavigation} />
           <Footer />
         </div>
       </Router>
--- react-client/src/Components/HomePage.jsx
+++ react-client/src/Components/HomePage.jsx
@@ -17,6 +17,18 @@ class HomePage extends Component {
           <p>
             That's what I look like if you were curious!
           </p>
+          <h2>
+            About Me
+          </h2>
+          <p>
+            Where to begin, I guess. Well for starters, I am Computer Science
+            student attending California Polytechnic State University or
+            Cal Poly for short. So this website, you are looking at is
+            programmed by me. Hold the applause though, it&apos; really not that
+             big of a deal. What about oher things I do? Glad you asked, I am in
+             the process of making a podcast getting coffee with my friends who
+             attend University, and that will be starting this fall. 
+          </p>
         </div>
 
       </div>
